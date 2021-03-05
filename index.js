bdaabaf232b8cb112c5f32c0fe55d886715bf467
Koren <koren@yunio.com>
2021-03-05 14:36:47 +0800

    新增index.js 和原生交互文件

--- index.js
+++ index.js
@@ -1,5 +1,124 @@
-import { NativeModules } from 'react-native';
+'use strict'
 
-const { Svga } = NativeModules;
+import React, { Component } from 'react'
+import { 
+    requireNativeComponent, 
+    Platform 
+} from 'react-native';
 
-export default Svga;
+const NativeSVGAPlayer = requireNativeComponent('RNSVGPlayer', RNSVGPlayer)
+
+class RNSVGPlayer extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    /** 
+     * 加载动画
+     * source 数据源
+     */
+    load(source) {
+        this.setState({
+            source
+        });
+    }
+
+    /** 开始动画 */
+    startAnimation() {
+        this.setState({
+            currentState: "start",
+        });
+    }
+
+    /** 暂停动画 */
+    pauseAnimation() {
+        this.setState({
+            currentState: "pause",
+        });
+    }
+
+    /** 停止动画 */
+    stopAnimation() {
+        this.setState({
+            currentState: "stop",
+        });
+    }
+
+    /** 
+     * 跳到某帧，然后继续（暂停）播放动画
+     * toFrame 指定跳到某一帧
+     * andPlay 播放状态
+     */
+    stepToFrame(toFrame, andPlay) {
+        this.setState({
+            currentState: andPlay === true ? "play" : "pause",
+            toFrame: -1,
+        }, () => {
+            this.setState({
+                toFrame,
+            });
+        });
+    }
+
+    /** 
+     * 跳到某进度，然后继续（暂停）播放动画
+     * toFrame 指定跳到某进度
+     * andPlay 播放状态
+     */
+    stepToPercentage(toPercentage, andPlay) {
+        this.setState({
+            currentState: andPlay === true ? "play" : "pause",
+            toPercentage: -1,
+        }, () => {
+            this.setState({
+                toPercentage,
+            });
+        });
+    }
+
+    render() {
+        let eventListeners = {};
+        if (Platform.OS === "android") {
+            eventListeners.onChange = (event) => {
+                const { action } = event.nativeEvent;
+                if (action === "onFinished") {
+                    if (typeof this.props.onFinished === "function") {
+                        this.props.onFinished();
+                    }
+                }
+                else if (action === "onFrame") {
+                    if (typeof this.props.onFrame === "function") {
+                        this.props.onFrame(event.nativeEvent.value);
+                    }
+                }
+                else if (action === "onPercentage") {
+                    if (typeof this.props.onPercentage === "function") {
+                        this.props.onPercentage(event.nativeEvent.value);
+                    }
+                }
+            }
+        }
+        else if (Platform.OS === "ios") {
+            if (typeof this.props.onFrame === "function") {
+                eventListeners.onFrame = (event) => {
+                    console.log(event);
+                    this.props.onFrame(event.nativeEvent.value);
+                }
+            }
+            if (typeof this.props.onPercentage === "function") {
+                eventListeners.onPercentage = (event) => {
+                    console.log(event);
+                    this.props.onPercentage(event.nativeEvent.value);
+                }
+            }
+        }
+
+        return (
+            <NativeSVGAPlayer {...this.props} {...this.state} {...eventListeners}/>
+        )
+    }
+}
+
+module.exports = RNSVGPlayer;
